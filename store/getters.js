0cd3ba50fe4c09457f2bcf7c894369d076e10e3d
kevin <wanghuan@silvrr.com>
2018-08-14 20:55:05 +0800

    fix

--- store/getters.js
+++ store/getters.js
@@ -3,7 +3,8 @@ export default {
   baseUrl() {
     let host
     if (process.env.NODE_ENV === 'production' && globalConfig.app.domain) {
-      host = `${globalConfig.app.domain}/${globalConfig.app.routerBaseApi}`
+      // host = `${globalConfig.app.domain}/${globalConfig.app.routerBaseApi}`
+      host = `http://${globalConfig.app.host}:${globalConfig.app.port}/${globalConfig.app.routerBaseApi}`
     } else {
       host = `http://${globalConfig.app.host}:${globalConfig.app.port}/${globalConfig.app.routerBaseApi}`
     }
